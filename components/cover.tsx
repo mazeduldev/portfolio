11a9ed71d245e5d6334f30017e227973cfbd6a4c
Mazedul Islam <mazidmailbox@gmail.com>
2021-03-17 13:32:13 +0600

    cover content and style updated

--- components/cover.tsx
+++ components/cover.tsx
@@ -4,7 +4,11 @@ import classes from './cover.module.scss'
 const Cover: React.FC = () => {
   return (
     <section className={classes.cover}>
-      
+      <div className={`container ${classes.coverContent}`}>
+        <div className={classes.text1}>Hello, my name is</div>
+        <div className={classes.text2}>Mazedul Islam</div>
+        <div className={classes.text3}>And I'm a <span>Software Engineer</span></div>
+      </div>
     </section>
   )
 }
