2492e3494d36ccaf72ab8f4b5568fceba3799cd9
Mazedul Islam <mazidmailbox@gmail.com>
2021-03-17 23:27:07 +0600

    typing animation added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.scss'
+import 'react-typist/dist/Typist.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
