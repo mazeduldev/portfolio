186207050a343cc8e69eab1f24c05e35e47bef86
Mazedul Islam <mazidmailbox@gmail.com>
2021-03-25 11:24:16 +0600

    card tilt and glare effect added

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { TiltOptions } from 'vanilla-tilt'
 import classes from './card.module.scss'
+import Tilt from './tilt'
 
 const Card = () => {
+  const tiltOptions: TiltOptions = {
+    glare: true,
+    "max-glare": 1
+  }
+
   return (
-    <div className={classes.card}>
-      <h2>Title</h2>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi quo iusto facilis, et laboriosam quia modi ipsum quam doloribus esse facere beatae obcaecati neque hic, quod delectus ab vel soluta corrupti?</p>
-    </div>
+    <Tilt className={classes.card} options={tiltOptions}>
+      <div className={classes.content}>
+        <h2>Title</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi quo iusto facilis, et laboriosam quia modi ipsum quam doloribus esse facere beatae obcaecati neque hic, quod delectus ab vel soluta corrupti?</p>
+      </div>
+    </Tilt>
   )
 }
 
