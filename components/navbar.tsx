cfe875a84228a436f39ad34a55e53d76adac2052
Mazedul Islam <mazidmailbox@gmail.com>
2021-03-28 03:11:53 +0600

    scroll to top button added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classes from './navbar.module.scss'
-import { Link, animateScroll as scroll } from "react-scroll";
+import { Link, animateScroll as scroll } from "react-scroll"
+import {FaArrowUp} from "react-icons/fa"
 
 const Navbar: React.FC = () => {
+  const [show, setShow] = useState("")
+
+  const showScrollButton = () => setShow(classes.show)
+
+  const hideScrollButton = () => setShow("")
+
   return (
-    <nav className={classes.navContainer}>
-      <ul className={classes.menu}>
-        <Link to="home" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Home</Link>
-        <Link to="portfolio" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Portfolio</Link>
-        <Link to="experience" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Experience</Link>
-        <Link to="about" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>About</Link>
-        <Link to="contact" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Contact</Link>
-      </ul>
-    </nav>
+    <>
+      <a onClick={scroll.scrollToTop} className={`${classes.scrollUpBtn} ${show}`}>
+        <FaArrowUp />
+      </a>
+      <nav className={classes.navContainer}>
+        <ul className={classes.menu}>
+          <Link to="home" activeClass={classes.active} smooth={true} spy={true} offset={-100} onSetActive={hideScrollButton} onSetInactive={showScrollButton} className={classes.menuItem}>Home</Link>
+          <Link to="portfolio" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Portfolio</Link>
+          <Link to="experience" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Experience</Link>
+          <Link to="about" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>About</Link>
+          <Link to="contact" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Contact</Link>
+        </ul>
+      </nav>
+    </>
   )
 }
 
