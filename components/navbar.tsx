587c7e2b53a6fdec2c9132caff93d2af21e19ab8
Mazedul Islam <mazidmailbox@gmail.com>
2021-03-28 03:26:20 +0600

    transparency added in nav bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,19 +5,34 @@ import {FaArrowUp} from "react-icons/fa"
 
 const Navbar: React.FC = () => {
   const [show, setShow] = useState("")
+  const [transparent, setTransparent] = useState(classes.bgTransparent)
 
   const showScrollButton = () => setShow(classes.show)
 
   const hideScrollButton = () => setShow("")
+
+  const makeTransparent = () => setTransparent(classes.bgTransparent)
+
+  const removeTransparency = () => setTransparent("")
+
+  const handleScrolledToTop = () => {
+    hideScrollButton()
+    makeTransparent()
+  }
+
+  const handleAwayFromTop = () => {
+    showScrollButton()
+    removeTransparency()
+  }
 
   return (
     <>
       <a onClick={scroll.scrollToTop} className={`${classes.scrollUpBtn} ${show}`}>
         <FaArrowUp />
       </a>
-      <nav className={classes.navContainer}>
+      <nav className={`${classes.navContainer} ${transparent}`}>
         <ul className={classes.menu}>
-          <Link to="home" activeClass={classes.active} smooth={true} spy={true} offset={-100} onSetActive={hideScrollButton} onSetInactive={showScrollButton} className={classes.menuItem}>Home</Link>
+          <Link to="home" activeClass={classes.active} smooth={true} spy={true} offset={-100} onSetActive={handleScrolledToTop} onSetInactive={handleAwayFromTop} className={classes.menuItem}>Home</Link>
           <Link to="portfolio" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Portfolio</Link>
           <Link to="experience" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Experience</Link>
           <Link to="about" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>About</Link>
