31dda2052de57445df33e012bf4f7a8bf40298bc
Mazedul Islam <mazidmailbox@gmail.com>
2021-04-08 23:20:38 +0600

    drawer navigation works

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -49,11 +49,11 @@ const Navbar: React.FC = () => {
       </nav>
       <Drawer isOpen={isDrawerOpen} toggle={toggleDrawer}>
         <div className={classes.drawerMenu}>
-          <Link to="home" smooth={true} spy={true} offset={-100} className={classes.menuItem}>Home</Link>
-          <Link to="portfolio" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Portfolio</Link>
-          <Link to="experience" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Experience</Link>
-          <Link to="about" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>About</Link>
-          <Link to="contact" activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Contact</Link>
+          <Link to="home" onClick={toggleDrawer} activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Home</Link>
+          <Link to="portfolio" onClick={toggleDrawer} activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Portfolio</Link>
+          <Link to="experience" onClick={toggleDrawer} activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Experience</Link>
+          <Link to="about" onClick={toggleDrawer} activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>About</Link>
+          <Link to="contact" onClick={toggleDrawer} activeClass={classes.active} smooth={true} spy={true} offset={-100} className={classes.menuItem}>Contact</Link>
         </div>
       </Drawer>
     </>
