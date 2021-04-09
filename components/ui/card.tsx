554c323835bdf5d7d2dfd2574ade6c3b6442cc83
Mazedul Islam <mazidmailbox@gmail.com>
2021-04-09 23:52:18 +0600

    sample data added in card component

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,22 +1,42 @@
-import React from 'react'
-import { TiltOptions } from 'vanilla-tilt'
-import classes from './card.module.scss'
-import Tilt from './tilt'
+import React from "react";
+import { TiltOptions } from "vanilla-tilt";
+import classes from "./card.module.scss";
+import Tilt from "./tilt";
+import Image from "next/image";
+import Button from "./button";
 
 const Card = () => {
   const tiltOptions: TiltOptions = {
     glare: true,
-    "max-glare": 1
-  }
+    "max-glare": 0.2,
+  };
 
   return (
     <Tilt className={classes.card} options={tiltOptions}>
+      <Image
+        src="/images/timeline_collage.png"
+        alt="timeline"
+        width={300}
+        height={200}
+        layout="intrinsic"
+        className={classes.roundedImage}
+      />
       <div className={classes.content}>
-        <h2>Title</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sequi quo iusto facilis, et laboriosam quia modi ipsum quam doloribus esse facere beatae obcaecati neque hic, quod delectus ab vel soluta corrupti?</p>
+        <h5 className={classes.cardTitle}>Vertical Timeline</h5>
+        <p className={classes.cardDescription}>
+          Angular material theme supported minimalist's vertical timeline library for Angular applications.
+        </p>
+        <div className={classes.cardTagContainer}>
+          <div className={classes.tag}>angular</div>
+          <div className={classes.tag}>angular-material</div>
+        </div>
+        <div className={classes.cardButtonContainer}>
+          <Button classes={classes.cardButton}><a href="https://github.com/mazid1/ngx-mzd-timeline" target="blank">Source Code</a></Button>
+          <Button classes={classes.cardButton}><a href="https://mazid1.github.io/ngx-mzd-timeline/" target="blank">Live Demo</a></Button>
+        </div>
       </div>
     </Tilt>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
