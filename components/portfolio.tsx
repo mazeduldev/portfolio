f1ceffda80abcabda8035bbdc449e57ed0f3194b
Mazedul Islam <mazidmailbox@gmail.com>
2021-04-11 22:56:51 +0600

    card component will take content as props.children

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
-import classes from './portfolio.module.scss'
-import Card from './ui/card'
+import React from "react";
+import classes from "./portfolio.module.scss";
+import Button from "./ui/button";
+import Card from "./ui/card";
+import Image from "next/image";
 
 const Portfolio = (props) => {
   return (
@@ -11,13 +13,166 @@ const Portfolio = (props) => {
           <h3 className="subtitle">My works</h3>
         </div>
         <div className={classes.content}>
-          <Card></Card>
-          <Card></Card>
-          <Card></Card>
+          <Card>
+            <Image
+              src="/images/timeline_collage.png"
+              alt="timeline"
+              width={300}
+              height={200}
+              layout="intrinsic"
+              className={classes.cardImage}
+            />
+            <div className={classes.cardBody}>
+              <h5 className={classes.cardTitle}>Vertical Timeline</h5>
+              <p className={classes.cardDescription}>
+                Angular material theme supported minimalist's vertical timeline
+                library for Angular applications.
+              </p>
+              <div className={classes.cardTagContainer}>
+                <div className={classes.tag}>angular</div>
+                <div className={classes.tag}>angular-material</div>
+              </div>
+              <div className={classes.cardButtonContainer}>
+                <Button classes={classes.cardButton}>
+                  <a
+                    href="https://github.com/mazid1/ngx-mzd-timeline"
+                    target="blank"
+                  >
+                    Source Code
+                  </a>
+                </Button>
+                <Button classes={classes.cardButton}>
+                  <a
+                    href="https://mazid1.github.io/ngx-mzd-timeline/"
+                    target="blank"
+                  >
+                    Live Demo
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </Card>
+          <Card>
+            <Image
+              src="/images/timeline_collage.png"
+              alt="timeline"
+              width={300}
+              height={200}
+              layout="intrinsic"
+              className={classes.cardImage}
+            />
+            <div className={classes.cardBody}>
+              <h5 className={classes.cardTitle}>Vertical Timeline</h5>
+              <p className={classes.cardDescription}>
+                Angular material theme supported minimalist's vertical timeline
+                library for Angular applications.
+              </p>
+              <div className={classes.cardTagContainer}>
+                <div className={classes.tag}>angular</div>
+                <div className={classes.tag}>angular-material</div>
+              </div>
+              <div className={classes.cardButtonContainer}>
+                <Button classes={classes.cardButton}>
+                  <a
+                    href="https://github.com/mazid1/ngx-mzd-timeline"
+                    target="blank"
+                  >
+                    Source Code
+                  </a>
+                </Button>
+                <Button classes={classes.cardButton}>
+                  <a
+                    href="https://mazid1.github.io/ngx-mzd-timeline/"
+                    target="blank"
+                  >
+                    Live Demo
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </Card>
+          <Card>
+            <Image
+              src="/images/timeline_collage.png"
+              alt="timeline"
+              width={300}
+              height={200}
+              layout="intrinsic"
+              className={classes.cardImage}
+            />
+            <div className={classes.cardBody}>
+              <h5 className={classes.cardTitle}>Vertical Timeline</h5>
+              <p className={classes.cardDescription}>
+                Angular material theme supported minimalist's vertical timeline
+                library for Angular applications.
+              </p>
+              <div className={classes.cardTagContainer}>
+                <div className={classes.tag}>angular</div>
+                <div className={classes.tag}>angular-material</div>
+              </div>
+              <div className={classes.cardButtonContainer}>
+                <Button classes={classes.cardButton}>
+                  <a
+                    href="https://github.com/mazid1/ngx-mzd-timeline"
+                    target="blank"
+                  >
+                    Source Code
+                  </a>
+                </Button>
+                <Button classes={classes.cardButton}>
+                  <a
+                    href="https://mazid1.github.io/ngx-mzd-timeline/"
+                    target="blank"
+                  >
+                    Live Demo
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </Card>
+          <Card>
+            <Image
+              src="/images/timeline_collage.png"
+              alt="timeline"
+              width={300}
+              height={200}
+              layout="intrinsic"
+              className={classes.cardImage}
+            />
+            <div className={classes.cardBody}>
+              <h5 className={classes.cardTitle}>Vertical Timeline</h5>
+              <p className={classes.cardDescription}>
+                Angular material theme supported minimalist's vertical timeline
+                library for Angular applications.
+              </p>
+              <div className={classes.cardTagContainer}>
+                <div className={classes.tag}>angular</div>
+                <div className={classes.tag}>angular-material</div>
+              </div>
+              <div className={classes.cardButtonContainer}>
+                <Button classes={classes.cardButton}>
+                  <a
+                    href="https://github.com/mazid1/ngx-mzd-timeline"
+                    target="blank"
+                  >
+                    Source Code
+                  </a>
+                </Button>
+                <Button classes={classes.cardButton}>
+                  <a
+                    href="https://mazid1.github.io/ngx-mzd-timeline/"
+                    target="blank"
+                  >
+                    Live Demo
+                  </a>
+                </Button>
+              </div>
+            </div>
+          </Card>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -2,39 +2,20 @@ import React from "react";
 import { TiltOptions } from "vanilla-tilt";
 import classes from "./card.module.scss";
 import Tilt from "./tilt";
-import Image from "next/image";
-import Button from "./button";
 
-const Card = () => {
-  const tiltOptions: TiltOptions = {
-    glare: true,
-    "max-glare": 0.2,
-  };
+const defaultTiltOptions: TiltOptions = {
+  glare: true,
+  "max-glare": 0.2,
+};
 
+const Card: React.FC<{ tiltOptions?: TiltOptions }> = ({
+  tiltOptions = defaultTiltOptions,
+  children,
+  ...restProps
+}) => {
   return (
-    <Tilt className={classes.card} options={tiltOptions}>
-      <Image
-        src="/images/timeline_collage.png"
-        alt="timeline"
-        width={300}
-        height={200}
-        layout="intrinsic"
-        className={classes.roundedImage}
-      />
-      <div className={classes.content}>
-        <h5 className={classes.cardTitle}>Vertical Timeline</h5>
-        <p className={classes.cardDescription}>
-          Angular material theme supported minimalist's vertical timeline library for Angular applications.
-        </p>
-        <div className={classes.cardTagContainer}>
-          <div className={classes.tag}>angular</div>
-          <div className={classes.tag}>angular-material</div>
-        </div>
-        <div className={classes.cardButtonContainer}>
-          <Button classes={classes.cardButton}><a href="https://github.com/mazid1/ngx-mzd-timeline" target="blank">Source Code</a></Button>
-          <Button classes={classes.cardButton}><a href="https://mazid1.github.io/ngx-mzd-timeline/" target="blank">Live Demo</a></Button>
-        </div>
-      </div>
+    <Tilt className={classes.card} options={tiltOptions} {...restProps}>
+      {children}
     </Tilt>
   );
 };
