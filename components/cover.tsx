9a6e620954108912f1dfbb99587afb52bec0a46e
Mazedul Islam <mazidmailbox@gmail.com>
2021-04-18 22:33:17 +0600

    bouncing arrow button added

--- components/cover.tsx
+++ components/cover.tsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import classes from './cover.module.scss'
-import TypingAnimation from './typing-animation'
+import React from "react";
+import { FaArrowDown } from "react-icons/fa";
+import { Link } from "react-scroll";
+import classes from "./cover.module.scss";
+import TypingAnimation from "./typing-animation";
 
-const myTitles = [
-  'Software Engineer',
-  'Web Developer',
-  'Fullstack Developer'
-]
+const myTitles = ["Software Engineer", "Web Developer", "Fullstack Developer"];
 
 const Cover = (props) => {
   return (
@@ -14,12 +12,20 @@ const Cover = (props) => {
       <div className={`container ${classes.coverContent}`}>
         <div className={classes.text1}>Hello, my name is</div>
         <div className={classes.text2}>Mazedul Islam</div>
-        <div className={classes.text3}>And I'm a
-          <TypingAnimation texts={myTitles}></TypingAnimation>
+        <div className={classes.text3}>
+          And I'm a<TypingAnimation texts={myTitles}></TypingAnimation>
         </div>
+        <Link
+          to="portfolio"
+          smooth={true}
+          offset={-64}
+          className={classes.arrow}
+        >
+          <FaArrowDown></FaArrowDown>
+        </Link>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Cover
+export default Cover;
